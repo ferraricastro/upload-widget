1a3377ab7ab2a8e631b717790f0b39d93414d7e2
ferraricastro <filipecastrosaraiva@gmail.com>
2026-04-14 15:42:08 -0300

    Initial commit

---
+++ web/src/components/upload-widget.tsx
@@ -0,0 +1,9 @@
+import { UploadWidgetHeader } from "./upload-widget-header";
+
+export function UploadWidget() {
+  return (
+    <div className="bg-zinc-900 max-w-360 rounded-3xl shadow-shape">
+      <UploadWidgetHeader />
+    </div>
+  );
+}
