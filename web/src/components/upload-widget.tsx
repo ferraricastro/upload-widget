25a15bccb6e2062bb277f4fae6987f0f07d0a295
ferraricastro <filipecastrosaraiva@gmail.com>
2026-04-22 15:05:44 -0300

    Animações do Widget

--- web/src/components/upload-widget.tsx
+++ web/src/components/upload-widget.tsx
@@ -2,30 +2,49 @@ import * as Collapsible from "@radix-ui/react-collapsible";
 import { UploadWidgetDropzone } from "./upload-widget-dropzone";
 import { UploadWidgetHeader } from "./upload-widget-header";
 import { UploadWidgetUploadList } from "./upload-widget-upload-list";
-import { useState } from "react";
+import { motion, useCycle } from "motion/react"
 import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
 
 export function UploadWidget() {
 
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+  const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true)
 
   return (
-    <Collapsible.Root onOpenChange={setIsWidgetOpen}>
-    <div className="bg-zinc-900 overflow-hidden w-90 rounded-xl shadow-shape">
-      {!isWidgetOpen && <UploadWidgetMinimizedButton />}
+    <Collapsible.Root onOpenChange={() => toggleWidgetOpen()}>
+      <motion.div 
+      className="bg-zinc-900 overflow-hidden w-90 rounded-xl shadow-shape"
+      animate={isWidgetOpen ? 'open' : 'closed'}
+      variants={{
+        closed: {
+          width: 'max-content',
+          height: 44,
+          transition: {
+            type: 'inertia'
+          },
+        },
+        open: {
+          width: 360,
+          height: 'auto',
+          transition: {
+            duration: 0.1,
+          },
+        },
+      }}
+      >
+        {!isWidgetOpen && <UploadWidgetMinimizedButton />}
 
-      <Collapsible.Content>
-        <UploadWidgetHeader />
-        
-        <div className="flex flex-col gap-4 py-3">
-          <UploadWidgetDropzone />
+        <Collapsible.Content>
+          <UploadWidgetHeader />
+          
+          <div className="flex flex-col gap-4 py-3">
+            <UploadWidgetDropzone />
 
-          <div className="h-px bg-zinc-800 border-t border-black/50 box-content"></div>
+            <div className="h-px bg-zinc-800 border-t border-black/50 box-content"></div>
 
-          <UploadWidgetUploadList />
-        </div>
-      </Collapsible.Content>  
-    </div>
+            <UploadWidgetUploadList />
+          </div>
+        </Collapsible.Content>  
+      </motion.div>
     </Collapsible.Root>  
   );
 }
